e233c8ec107335cf756094bcfb9d87cb8eca48a1
Kevin Tran <19kevintran@ou.edu>
2024-05-10 23:29:23 -0500

    background img

---
+++ src/components/HeroImg.js
@@ -0,0 +1,25 @@
+import "./HeroImgStyles.css";
+import React from "react";
+import IntroImg from "../assets/backgroundimg.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>Hi, I'm Kevin Tran</p>
+        <h1>Full Stack Developer</h1>
+        <div>
+            <Link to ="/project" className="btn"> Project</Link>
+            <Link to ="/contact" className="btn-light"> Contact</Link>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
