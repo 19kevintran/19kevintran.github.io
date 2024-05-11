d072a4b2a8629a1bfe8ee4b6af19efed11b7f732
Kevin Tran <19kevintran@ou.edu>
2024-05-11 01:01:53 -0500

    Fixed bg image sizing

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,13 +1,13 @@
 import "./HeroImgStyles.css";
 import React from "react";
-import IntroImg from "../assets/backgroundimg.jpg";
+import IntroImg from "../assets/background.jpeg";
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
       <div className="mask">
-        <img className="into-img" src={IntroImg} alt="IntroImg" />
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
       </div>
       <div className="content">
         <p>Hi, I'm Kevin Tran</p>
