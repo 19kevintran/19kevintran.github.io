907cfeed432c1ae22f375160ee661022b59e2587
Kevin Tran <19kevintran@ou.edu>
2024-05-14 08:54:08 -0500

    Fixing project alignment

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,12 +2,17 @@ import "./AboutContent.css";
 import React from "react";
 import React1 from "../assets/backgroundimg.jpg";
 import CageApp from "../assets/CageApp.png";
+import Kevin from "../assets/Kevin.PNG";
+
 
 import { Link } from "react-router-dom";
 const AboutContent = () => {
   return (
     <div className="about">
       <div className="left">
+      <div className="profile-image">
+          <img src={Kevin} className="img" alt="Kevin Tran" />
+        </div>
         <h1>Who Am I?</h1>
         <p>I'm am a full stack developer creating a react Portfolio</p>
         <Link to="/contact">
